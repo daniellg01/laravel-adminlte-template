39350b4d5be79f071f0f458ee7ff80af17b80198
daniellg01 <lozanodevelopment01@gmail.com>
2024-02-12 16:07:14 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#composer install
+#php artisan key:generate
+#php artisan optimize
+#php artisan adminlte:plugins install
---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,26 @@
+@extends('adminlte::auth.login')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
